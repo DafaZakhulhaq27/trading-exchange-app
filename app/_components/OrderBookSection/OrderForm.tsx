939891406538eb291b391553c9ab0782962c5dd9
Dafa Zakhulhaq <dafa27890@gmail.com>
2024-06-04 07:40:12 +0700

    feat: add dropdown component

--- app/_components/OrderBookSection/OrderForm.tsx
+++ app/_components/OrderBookSection/OrderForm.tsx
@@ -1,13 +1,39 @@
-import { ThemedText } from "@/components/ThemedText";
 import { View } from "react-native";
 import ButtonBuySell from "./ButtonBuySell";
+import Select from "@/components/Forms/Select";
+import { StyleSheet } from "react-native";
+import { useState } from "react";
+
+const TypeBooks = [
+  {
+    title: "Limit Order",
+    value: "limit-order",
+  },
+  {
+    title: "Market Order",
+    value: "market-order",
+  },
+];
 
 export default function OrderForm() {
-  return (
-    <View>
-      <ButtonBuySell onChange={() => {}} />
+  const [selectedTypeBook, setSelectedTypeBook] = useState(TypeBooks[0]);
 
-      <ThemedText>Order Form</ThemedText>
+  return (
+    <View style={styles.formContainer}>
+      <ButtonBuySell onChange={() => {}} />
+      <Select
+        defaultValue={TypeBooks[0]}
+        onSelect={(selectedItem) => {
+          setSelectedTypeBook(selectedItem);
+        }}
+        data={TypeBooks}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  formContainer: {
+    gap: 10,
+  },
+});
---
+++ components/Forms/Select.tsx
@@ -0,0 +1,70 @@
+import { ThemedText } from "@/components/ThemedText";
+import { View } from "react-native";
+import SelectDropdown, {
+  SelectDropdownProps,
+} from "react-native-select-dropdown";
+import Flex from "@/components/Flex";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
+import { Colors } from "@/constants/Styles";
+
+export type SelectProps = {
+  data: { title: string; value: string }[];
+} & Omit<SelectDropdownProps, "renderButton" | "renderItem">;
+
+export default function Select({ data, ...props }: SelectProps) {
+  return (
+    <SelectDropdown
+      {...props}
+      data={data}
+      renderButton={(selectedItem, isOpened) => {
+        return (
+          <Flex style={styles.selectButton}>
+            <ThemedText style={styles.selectButtonText}>
+              {selectedItem?.title}
+            </ThemedText>
+            <Ionicons name={isOpened ? "chevron-up" : "chevron-down"} />
+          </Flex>
+        );
+      }}
+      renderItem={(item) => {
+        return (
+          <View style={styles.renderItem}>
+            <ThemedText>{item.title}</ThemedText>
+          </View>
+        );
+      }}
+      showsVerticalScrollIndicator={false}
+      dropdownStyle={styles.dropdownMenuStyle}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  dropdownMenuStyle: {
+    backgroundColor: "#E9ECEF",
+    borderRadius: 8,
+    marginTop: -30,
+  },
+  selectButton: {
+    height: 28,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: Colors.main.grey,
+    width: "100%",
+  },
+  selectButtonText: {
+    fontWeight: 600,
+  },
+  renderItem: {
+    height: 28,
+    paddingHorizontal: 10,
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: Colors.main.grey,
+  },
+});
